dfa258ce8f61d842026391d23c79beb044de514a
HODUCVU <hoducvu1234@gmail.com>
2024-02-19 18:14:28 +0700

    Modify MQ135

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
 
+// Include modules
+#include <DHT22.h>
+#include <MQ135m.h>
+#include <LCD16x2.h>
+#include <Fan.h>
+#include <Lamp.h>
+
 
 
 void setup() {}
